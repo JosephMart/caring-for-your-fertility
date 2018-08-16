a4cf55d6f2769d31f3466f1523e7d856398de337
Joseph Martinsen <joseph@martinsen.com>
2018-08-15 23:50:04 -0500

    Added Spanish Information Text

--- src/containers/Info/Container.js
+++ src/containers/Info/Container.js
@@ -1,120 +1,31 @@
 import React, { Component } from 'react';
-import './styles.css';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class Info extends Component {
+import EnglishText from './English';
+import SpanishText from './Spanish';
+
+import './styles.css';
+import { SPANISH } from '../../constants';
+
+class Info extends Component {
+    static propTypes = {
+        language: PropTypes.string.isRequired,
+    }
+
     render() {
-        const FertilityCare = <b>Fertility<i>Care</i></b>;
-        const tradeMark = <span>™</span>;
+        const text = this.props.language === SPANISH ? SpanishText : EnglishText;
+
         return (
             <section className="info-page">
-                <div className="wrapper">
-                    <h1>{FertilityCare}{tradeMark} System</h1>
-
-                    <h2>{FertilityCare}{tradeMark} System Facts</h2>
-                    <p>
-                        The {FertilityCare}{tradeMark} System teaches a woman and couple to identify
-                        the fertile and infertile days in her cycle. The couple then uses
-                        this information to plan or avoid a pregnancy. The System is based
-                        on the observation and tracking of physical signs a woman has when
-                        she is fertile.
-                    </p>
-                    <p>
-                        The system has a 99.6% degree of effectiveness to avoid pregnancy
-                        and a very high degree of accuracy in achieving pregnancy.
-                        A Meta Analysis of five effectiveness studies was
-                        published in the "Journal of Reproductive Medicine"
-                    </p>
-
-                    <h2>The {FertilityCare}{tradeMark} System is more than Natural Birth Control</h2>
-                    <p>
-                        Couples report the {FertilityCare}{tradeMark} System is so much more than natural birth
-                        control. Birth control has only one purpose, preventing pregnancy.
-                        The {FertilityCare}{tradeMark} System is also very effective to achieve pregnancy. As an
-                        additional benefit, couples report that their communication increases.
-                        Women express appreciation that this method also involves the couple
-                    </p>
-                    <p>
-                        {FertilityCare}{tradeMark} System works in union with NaProtechnology helping women/couples with:
-                    </p>
-                    <ul>
-                        <li>Family planning</li>
-                        <li>Chronic discharges</li>
-                        <li>Targeted hormone evaluation</li>
-                        <li>Targeted hormone replacement</li>
-                        <li>Identify ovarian cysts</li>
-                        <li>The effects of stress</li>
-                        <li>Infertility</li>
-                        <li>Miscarriages</li>
-                        <li>Premenstrual syndrome</li>
-                        <li>Chronic infections</li>
-                        <li>Other reproductive disorders</li>
-                        <li>Prematurity prevention</li>
-                        <li>Abnormal bleeding</li>
-                        <li>And more...</li>
-                    </ul>
-
-                    <h2>CREIGHTON MODEL <br /> {FertilityCare}{tradeMark} <i>System and Infertility</i></h2>
-                    <p>
-                        {FertilityCare}{tradeMark} System provides new hope for couples who are going
-                        through the experience of infertility or low fertility. Not only does this
-                        system inform couples of when they are most fertile, it also aids in identifying
-                        factors that may be contributing to infertility. Women no longer rely on the
-                        calendar to determine where they are in their cycle resulting in more accurate diagnostic testing.
-                    </p>
-
-                    <h2>{FertilityCare}{tradeMark} System is easy to learn</h2>
-                    <p>
-                        The first step to learning the method is to attend an Introductory Session. These Sessions
-                        can also be scheduled to accommodate an individual's needs and schedule. At the end of this
-                        Session the couple decides if they want to continue with instruction. Couples are then given a
-                        Personal Fertility Kit that contains a book, chart, and everything they need to begin tracking
-                        of their fertility. There is a _______ fee for the kit.
-                    </p>
-                    <p>
-                        The couple is then scheduled for a Follow-Up appointment. These sessions are private appointments
-                        structured for the purpose of support, learning, reviewing charting, and assessing instructions.
-                    </p>
-                    <p>
-                        There is a fee of _____ per session. The Follow-Up sessions are every two weeks during the first
-                        two months of learning and then are less frequent after that. There are 8 Follow-up sessions over
-                        a one-year period. All services are private and confidential. Daytime and evening hours are available.
-                        Long distance learning is available through phone, mail, fax, or e-mail when it is not possible to come
-                        in person for instruction. The method is affordable to anyone and may be reimbursed by some HMOs and
-                        third party payers.
-                    </p>
-
-                    <h2> Basic Facts</h2>
-                    <ul>
-                        <li>Regular cycles are not necessary to use the FertilityCare™ System</li>
-                        <li>Women ovulate during one 24-hour period in each cycle and he egg lives for only 12 to 24 hours if there is no conception.</li>
-                        <li>Women will experience a very definite symptom when they ovulate and for several days before, this is the time when the couple is fertile and pregnancy can occur. A woman is taught how to observe and interpret this biomarker and together the couple is taught how to use the system.</li>
-                        <li>By observing and tracking this biomarker the couple can determine the beginning and end of fertility regardless of cycle regularity.</li>
-                        <li>A woman, who is breastfeeding, coming off oral contraceptives, in long cycles, or in pre-menopause can use the method with a very high degree of effectiveness. It was often thought impossible to use a natural method in these reproductive categories.</li>
-                    </ul>
-
-                    <h2>FertilityCare System is the First Step to NaPro<b>TECHNOLOGY</b>®.</h2>
-                    <p>
-                        Through NaPro<b>TECHNOLOGY®</b>, a new reproductive science that has its
-                        roots in the CREIGHTON MODEL {FertilityCare}{tradeMark} System, Physicians, Nurse Practitioners, Physician Assistants and
-                        Midwives can assist women with infertility, repeated miscarriages, PMS, Ovarian cysts, menopause, and a variety of reproductive issues. The standardized charting provides a tool to identify indicators of problems. When these are identified treatments can be prescribed that are founded in an understanding of what is actually occurring in the woman's cycle.
-                    </p>
-
-                    <h2>Couples report that the FertilityCare System benefits the relationship</h2>
-                    <p>Couples report an increase in communication and a growth in the</p>
-                    <p><b>S</b>piritual</p>
-                    <p><b>P</b>hysical</p>
-                    <p><b>I</b>ntellectual,</p>
-                    <p><b>C</b>ommunicative/ Creative</p>
-                    <p><b>E</b>motional dimensions their sexuality and relationship.</p>
-                    <div className="center">
-                        <h3>For more information on the</h3>
-                        <h3>CREIGHTON MODEL</h3>
-                        <h3>FertilityCare System go to:</h3>
-                        <h3><a href="http://www.fertilitycare.org" target="__blank">www.fertilitycare.org</a></h3>
-                        <h3><a href="http://www.creightonmodel.com" target="__blank">www.creightonmodel.com</a></h3>
-                    </div>
-                </div>
+                {text}
             </section>
         );
     }
 }
+
+const mapStateToProps = (state, ownProps) => ({
+    language: state.language,
+});
+
+export default connect(mapStateToProps)(Info);
---
+++ src/containers/Info/Spanish.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { TM, FC } from '../../constants';
+
+export default (
+    <div className="wrapper">
+        <h1>{FC}{TM} System</h1>
+
+        <h2>{FC}{TM} System Información</h2>
+
+        <p>
+            Las parejas que lo han usado reportanque el {FC}{TM} System es mucho más que
+            un control natural de la natalidad. El control de la natalidad tiene un solo propósito:
+            prevenir el embarazo.
+        </p>
+        <p>
+            El {FC}{TM} System también es muy efectivo para lograr el embarazo.
+            Como beneficio adicional, las parejas informaron que, desde su uso, su comunicación aumentó.
+            Las mujeres expresan su aprecio a este sistema ya que involucra a la pareja y ayuda a entender
+            el cuerpo de la mujer.
+        </p>
+
+        <h2>
+            Los Profesionales del Cuidado de la Fertilidad ({FC}{TM} Pracitioners) y los Practicantes
+            (Practitioner Interns) son los únicos que pueden enseñar el {FC}{TM} System
+        </h2>
+
+        <p>
+            El <b>Profesional del Creighton Model</b> {FC}{TM} System o el <b>Pracicante</b> es un Profesionalde la salud afiliado que se encuantra en, o que ha completado, una preparación teórica y de experiencia
+            clínica de 13 meses. El <b>Practicante del Cuidado de la Fertilidad</b> enseña a mujeres y parejas
+            el Sistema bajo la supervisión de un <b>Educador del Cuidado de la Fertilidad</b> certificado.
+        </p>
+
+        <h2>Hechos Básicos</h2>
+
+        <ul>
+            <li>
+                No es necesario tener ciclos regulares para usar el {FC}{TM} System
+            </li>
+            <li>
+                Las mujeres ovulan durante un período de 24 horas en cada ciclo y el óvulo vive sólo de 12
+                a 24 horas si no hay concepción
+            </li>
+            <li>
+                Las mujeres experimentan un síntoma mu definido cuando ovulan y durante varios días antes,
+                este es el momento en que la pareja es fértil y el embarazo puede ocurrir. A la mujer se le enseña
+                cómo observar e interpretar este marcador biologico y se enseña a la pareja a usar el sistema
+                de manera conjunta
+            </li>
+            <li>
+                Al observar y seguir este marcador biologico, a pareja puede determinar el inicio y el final de la
+                fertilidad independientemente de la regularidad del ciclo
+            </li>
+            <li>
+                Una mujer que está amamantando, dejando los anticonceptivos orales, que tiene ciclos largos o se
+                encuentra en la pre-menopausia puede utilizar el método con un grado muy alto de efectividad. A
+                menudo se creía imposible utilizar un método natural en estas categorías reproductivas.
+            </li>
+        </ul>
+
+        <h2>
+            El  {FC}{TM} System es el primer paso hacia la <i>NaPro</i>TECNOLOGÍA (<i>NaPro</i>TECHNOLOGY&reg;)
+        </h2>
+
+        <p>
+            A través de la <i>NaPro</i><b>TECNOLOGÍA</b>, una nueva ciencia reproduciva que tiene sus raíces en el
+            <b>CREIGHTON MODEL</b> {FC}{TM} System, médicos, enfermeras, asistentes médicos y parteras pueden ayudar
+            a las mujeres con infertilidad, abortos espontáneos frecuentes, Síndrome Premenstrual, quistes ováricos,
+            menopausia y una variedad de problemas reproductivos. La graficación estandarizada proporciona una
+            herramienta para identificar indicadores de problemas. Cuando éstos se identifican, se pueden prescribir
+            los tratamientos adecuados que se basan en una compresión de lo que realmente está ocurriendo en el ciclo
+            de la mujer
+        </p>
+
+        <h2>
+            Las Parejas reportan que el {FC}{TM} System beneficia la relación
+        </h2>
+
+        <p>
+            Las parejas informan un incremento en la comunicación y un crecimiento en las dimensiones:
+        </p>
+        
+        <p><b style={{"font-size": "22px"}}>S</b>piritual - Espiritual</p>
+        <p><b style={{"font-size": "22px"}}>P</b>hysical - Física</p>
+        <p><b style={{"font-size": "22px"}}>I</b>ntellectual - Intelectual</p>
+        <p><b style={{"font-size": "22px"}}>C</b>ommunicative - Comunicativa/Creativa</p>
+        <p><b style={{"font-size": "22px"}}>E</b>motional - Emocional</p>
+
+        <p>
+            Éstas son dimensiones de la sexualidad de la pareja, así como de su relación.
+        </p>
+
+        <div className="center">
+            <h3>Para mas informatíon acerca de</h3>
+            <h3>CREIGHTON MODEL</h3>
+            <h3>{FC}{TM} System:</h3>
+            <h3><a href="http://www.FC.org" target="__blank">www.FC.org</a></h3>
+            <h3><a href="http://www.creightonmodel.com" target="__blank">www.creightonmodel.com</a></h3>
+        </div>
+    </div>
+);
